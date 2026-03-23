21be1e7c938e6bf02a9b1a01864e9ebc0d3ebc36
Udaya Adhikari <adhikariudaya521@gmail.com>
2026-03-23 11:09:22 +0545

    small change on the api.php page

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\ServiceContractController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+    Route::post('/ourpayments', [PaymentController::class, 'store']);
+    Route::post('/ourservicecontracts', [ServiceContractController::class, 'store']);
+
